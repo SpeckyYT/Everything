60c0052529f6d258b5334f8e078806bdfb3e1645
theo <72149158+SynergyBest@users.noreply.github.com>
2021-05-05 07:51:24 -0700

    Create Everything.cs

---
+++ Everything.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Everything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Everything");
+        }
+    }
+}
