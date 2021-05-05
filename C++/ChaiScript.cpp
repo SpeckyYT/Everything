a07e50a129d059d836565ac9368ea86e585691a0
theo <72149158+SynergyBest@users.noreply.github.com>
2021-05-05 14:43:43 -0500

    Update ChaiScript.cpp

--- C++/ChaiScript.cpp
+++ C++/ChaiScript.cpp
@@ -1,12 +1,12 @@
 #include <chaiscript/chaiscript.hpp>
 
-std::string helloWorld(const std::string &t_name) {
+std::string everyThing(const std::string &t_name) {
   return "ever " + t_name + "ing";
 }
 
 int main() {
   chaiscript::ChaiScript chai;
-  chai.add(chaiscript::fun(&helloWorld), "everyThing");
+  chai.add(chaiscript::fun(&everyThing), "everyThing");
 
   chai.eval(R"(
     puts(everyThing("yth"));
